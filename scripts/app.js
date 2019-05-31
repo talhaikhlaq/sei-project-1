45e05b216d347b4ac8059a6224afc7e19ff6cfb0
TalhaIkhlaq <talhaikhlaq@gmail.com>
2019-05-31 12:19:48 +0100

    player ship placed at bottom of grid and only moving left or right.

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,55 @@
-console.log('Hello')
+const width = 10
+const squares = []
+let playerIndex = Math.floor(width * width - width) // will keep player at the bottom of the gameBoard
+
+
+function movePlayer() {
+  squares.forEach(square => square.classList.remove('player')) // removing the classList associated to the player image
+  squares[playerIndex].classList.add('player') // adding the classList associated to the player image
+
+}
+
+
+function handleKeyDown(e) {
+  let playerShouldMove = true
+  switch(e.keyCode) {
+    case 39:
+      if (playerIndex % width < width -1) {
+        playerIndex++
+      }
+      break
+    case 37:
+      if (playerIndex % width > 0) {
+        playerIndex--
+      }
+      break
+    default:
+      playerShouldMove = false
+  }
+  if (playerShouldMove) movePlayer()
+}
+
+
+function init() {
+
+  // get hold of that parent grid div
+  const grid = document.querySelector('.grid')
+
+
+  // used a for loop to fill my grid with individual squares, as many as the width times the width
+  for (let i = 0; i < width * width; i++) {
+    const square = document.createElement('div')
+    square.classList.add('grid-item')
+    square.innerHTML = i
+    squares.push(square)
+    grid.append(square)
+  }
+
+  // adds the player image to the squares
+  squares[playerIndex].classList.add('player')
+
+  window.addEventListener('keydown', handleKeyDown)
+
+}
+
+window.addEventListener('DOMContentLoaded', init)
